27d081e7d166af5c3a3e64283a4eece60d9fd72b
LucaKuechler <lucamax2001@gmx.de>
2020-06-01 20:04:22 +0200

    styling
    worked on template & added icons

--- C# Builder/obj/Debug/Shelly_Content.g.i.cs
+++ C# Builder/obj/Debug/Shelly_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/images/system/logo.ico")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/images/system/logo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/unbenannt.png")]
 
 
