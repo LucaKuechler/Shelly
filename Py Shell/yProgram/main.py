8f15a1e8ffaed3c058dc903acc623afb54f7aeaf
LucaKuechler <LucaKuechler.Business@gmail.com>
2020-06-19 12:28:03 +0200

    *

---
+++ Py Shell/yProgram/main.py
@@ -0,0 +1,29 @@
+import cmd
+import shell
+#import commandList
+
+#Main
+class Program(shell.Shell):
+
+    ''' system function '''
+
+    def main(self, list_input):
+        print(list_input)
+
+
+
+if __name__ == '__main__':
+    while True:
+        Object = Program()
+        status = Object.call_shell()
+        list_input = Object.get_list()
+        list_struc = Object.get_struc()
+
+        if status == False:
+            break
+        else:
+            Program().main(list_input)
+
+
+
+    
